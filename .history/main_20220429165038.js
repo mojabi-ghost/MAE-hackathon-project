1d6638d482231ce756c75fc66b2d025c735583a7
Alex Maddux <alexmaddux99@gmail.com>
2022-04-29 16:55:38 -0700

    reverted to older version

--- .history/main_20220429165038.js
+++ .history/main_20220429165038.js
@@ -82,3 +82,41 @@ function changeSlide(e) {
     
   }
   
+const DC = [
+    "/images/aquaman.jpg",
+    "/images/batman.jpg",
+    "/images/cyborg.jpg",
+    "/images/flash.jpg",
+    "/images/superman.jpg",
+    "/images/wonderWoman.jpg",
+    ]
+
+function get_current_image_index(current_image){
+    for (let i = 0; i < DC.length; i++) {
+        if (current_image.src.substring(21) == DC[i]){
+            return i;
+        }
+    }
+}
+
+function next_image() {
+    let current_image = document.querySelector("img");
+    i = get_current_image_index(current_image);
+    if (i + 1 == DC.length){
+        current_image.src = current_image.src.substring(0, 21) + DC[0];
+    }
+    else{
+        current_image.src = current_image.src.substring(0, 21) + DC[i + 1];
+    }
+}
+
+function prev_image(){
+    let current_image = document.querySelector('img');
+    i = get_current_image_index(current_image);
+    if (i - 1 < 0){
+        current_image.src = current_image.src.substring(0, 21) + DC[DC.length - 1];
+    }
+    else{
+        current_image.src = current_image.src.substring(0, 21) + DC[i - 1];
+    }
+}
