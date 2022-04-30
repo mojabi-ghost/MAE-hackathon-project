fee0586ef1a0ab89ef2cb51dd7743df3c09b1ac7
Enjie Huang <ehuang222@insite.4cd.edu>
2022-04-30 10:49:22 -0700

    everthing works but the fade animation

--- main.js
+++ main.js
@@ -1,71 +1,98 @@
 //Javascript
 
-document.addEventListener('DOMContentLoaded', init);
-var myInterval = "";
+//var myInterval = setInterval(next_image, 5000);
+
+const links = [
+  "https://en.wikipedia.org/wiki/Aquaman",
+  "https://en.wikipedia.org/wiki/Batman", 
+  "https://en.wikipedia.org/wiki/Cyborg_(DC_Comics)", 
+  "https://en.wikipedia.org/wiki/Flash_(DC_Comics_character)",
+  "https://en.wikipedia.org/wiki/Superman",
+  "https://en.wikipedia.org/wiki/Wonder_Woman"
+]
+
+const image_location = [
+  "images/aquaman.jpg",
+  "images/batman.jpg",
+  "images/cyborg.jpg",
+  "images/flash.jpg",
+  "images/superman.jpg",
+  "images/wonderWoman.jpg",
+]
+
+const captions = [
+  "Aquaman (1/6)",
+  "Batman (2/6)",
+  "Cyborg (3/6)",
+  "The Flash (4/6)",
+  "Superman (5/6)",
+  "Wonderwoman (6/6)",
+]
  
-function init() {
-  //create shortcut vars
-  const back_btn = document.querySelector(".back-btn");
-  const next_btn = document.querySelector(".next-btn");
-  const frame = document.querySelector(".frame");
-  const slides = frame.querySelectorAll("img");
-  const caption = document.querySelector(".caption");
-  const controls = document.querySelector(".controls");
-  
+//create shortcut vars
+const frame = document.querySelector(".frame");
+const current_links = document.getElementsByClassName("wikiLink");
+const slides = frame.querySelectorAll("img");
+const altName = frame.querySelectorAll("alt");
+const caption = document.querySelector(".caption");  
 
-  //with JS active, hide all images
-  slides.forEach((slide) => {
-    slide.classList.add("hide", "abs-pos");
-  });
-  
-  // show the first slide
-  slides[0].classList.remove("hide");
+//with JS active, hide all images
+slides.forEach((slide) => {
+  slide.classList.add("hide", "abs-pos", "current");
+});
 
-   // set the caption dynamically
-   caption.innerHTML = frame.firstElementChild.alt;
+// show the first slide
+slides[0].classList.remove("hide");
+set_attributes(0);
+caption.innerHTML = captions[0];
 
-   //show the controls
-   controls.style.display = "block";
-
-   myInterval = setInterval(changeSlide, 5000);
+function set_attributes(i){
+  for(let j = 0; j < current_links.length; j++){
+    current_links[j].setAttribute("href", links[i]);
+    slides[j].setAttribute("alt", captions[i]);
+    slides[j].setAttribute("src", image_location[i]);
+  }
 }
 
-const DC = [
-    "/images/aquaman.jpg",
-    "/images/batman.jpg",
-    "/images/cyborg.jpg",
-    "/images/flash.jpg",
-    "/images/superman.jpg",
-    "/images/wonderWoman.jpg",
-    ]
-
-function get_current_image_index(current_image){
-    for (let i = 0; i < DC.length; i++) {
-        if (current_image.src.substring(21) == DC[i]){
-            return i;
-        }
+function get_current_image_index(){
+  for(let i = 0; i < captions.length; i++){
+    if(slides[0].alt == captions[i]){
+      return i;
     }
+  }
 }
 
-function next_image() {
-    let current_image = document.querySelector("img");
-    console.log(current_image)
-    i = get_current_image_index(current_image);
-    if (i + 1 == DC.length){
-        current_image.src = current_image.src.substring(0, 21) + DC[0];
-    }
-    else{
-        current_image.src = current_image.src.substring(0, 21) + DC[i + 1];
-    }
+function change_image(){
+  // deactivate current image
+  slides[0].classList.add("hide");
+
+  // activate next image
+  slides[1].classList.remove("hide");
+
+  //change caption text
+  caption.innerHTML = slides[0].alt;
+}
+
+function next_image(){
+  i = get_current_image_index();
+  if(i + 1 > 5){
+    set_attributes(0)
+  }
+  else{
+    set_attributes(i + 1)
+  }
+  change_image();
+  
 }
 
 function prev_image(){
-    let current_image = document.querySelector('img');
-    i = get_current_image_index(current_image);
-    if (i - 1 < 0){
-        current_image.src = current_image.src.substring(0, 21) + DC[DC.length - 1];
-    }
-    else{
-        current_image.src = current_image.src.substring(0, 21) + DC[i - 1];
-    }
+  i = get_current_image_index();
+  if(i - 1 < 0){
+    set_attributes(captions.length - 1)
+  }
+  else{
+    set_attributes(i - 1)
+  }
+  change_image();
+  
 }
